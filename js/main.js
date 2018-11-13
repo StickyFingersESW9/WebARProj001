9c45eae3e58afa8b46b4c45a436d1f38088da64f
StickeyFingersESW9 <koiwai.1059373.me03sf.ilohas@gmail.com>
2018-11-13 11:05:47 +0900

    modify main.

--- js/main.js
+++ js/main.js
@@ -157,14 +157,14 @@ function init() {
     var stampAttay = [];
     var stamp00 = CreatePolygon( new THREE.TextureLoader().load( stumpPath[5] ) );
     stamp00.position.set( 0, 0, 1000 );
-    stamp00.scale.set( 0.25, 0.25, 0.25 );
+    //stamp00.scale.set( 0.25, 0.25, 0.25 );
     scene.add( stamp00 );
     stampAttay.push( stamp00 );
 
     var stamp01 = CreatePolygon( new THREE.TextureLoader().load( stumpPath[3] ) );
     stamp01.position.set( 0, 0, -1000 );
-    stamp01.rotation.set( 0, 180, 0 );
-    stamp01.scale.set( 0.25, 0.25, 0.25 );
+    stamp01.rotation.set( 0, 180 * (Math.PI / 180), 0 );
+    //stamp01.scale.set( 0.25, 0.25, 0.25 );
     scene.add( stamp01 );
     stampAttay.push( stamp01 );
 
